ed0c8e4bba9ca71f568b700d6a06df19338cc3ce
Giovanni Fiori <giovannicfiori@gmail.com>
2022-03-22 23:09:12 -0400

    Implement Hero section as component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import profilePic from '../public/images/profile.jpg';
+import Hero from '../components/Hero'
 import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
@@ -14,20 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.contentBlock}>
-          <div className={styles.imageContainer}>
-            <Image src={profilePic} alt="Picture of the author" layout="fill" objectFit="cover" />
-          </div>
-          <div className={styles.contentInfo}>
-            <p><b>Hello!</b> I'm</p>
-            <span className={styles.name}>Giovanni Fiori</span>
-            <p>A Software Engineer with more than 4 years of experience, based in Brazil;</p>
-            <p>Specialized in Web but has also worked with Mobile;</p>
-            <p>Graduated in Software Engineering from IComp - Universidade Federal do Amazonas;</p>
-            <p>Currently is a Software Engineer at Jusbrasil connecting people to justice.</p>
-          </div>
-        </div>
-        
+        <Hero />
       </main>
     </>
   )
