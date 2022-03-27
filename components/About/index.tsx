bd5e74d067e5eb1b60d5d2a73e097eeeb13eca4c
Giovanni Fiori <giovannicfiori@gmail.com>
2022-03-27 12:05:26 -0400

    Add About section

---
+++ components/About/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Section from '../HomeSection'
+
+import styles from './About.module.scss'
+
+const About = () => (
+    <Section title="about">
+        <div className={styles.textContent}>
+            <p>My name is Giovanni Fiori. I&apos;m 24 years old and I live in Brazil. I have a Bachelor&apos;s degree in
+                Software Engineering from IComp - Universidade Federal do Amazonas.</p>
+
+            <p>Currently I&apos;m a Fullstack Software Engineer at Jusbrasil helping to reduce the justice gap in Brazil
+                by providing meaningful and quality information to millions of people.</p>
+
+            <p>I have more than 4 years of experience in Software Engineering, where the majority of the time I&apos;ve
+                developed web apps and APIs. I also had a lot of experience in projects for Mobile, specifically for the
+                Android platform when working for a Samsung&apos;s R&D institute.</p>
+
+            <p>You can see more about my skills on the next sections &#x1f609;</p>
+        </div>
+    </Section>
+)
+
+export default About
